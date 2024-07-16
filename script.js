68b2084612ca8d369779924e78f37d3327e3fa6b
NrNSol <nairondev@outlook.com>
2024-07-15 21:46:46 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toogleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a img
+  if (html.classList.contains("light")) {
+    // se conter light mode, adicionar a img light
+    img.setAttribute("src", "./assets/lightimg.png")
+    } else {
+    // se conter sem light mode, manter imagem padrão
+    img.setAttribute("src", "./assets/ondaimg.png")
+  }
+}
